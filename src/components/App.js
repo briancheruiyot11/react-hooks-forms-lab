932831b386ee41a115131dc949c8b170b7412e82
Brian Cheruiyot <briancheru11@gmail.com>
2025-07-26 09:24:38 +0300

    refactor: update App component to manage filtered items and search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,34 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  const filteredItems = items.filter((item) => {
+    return (
+      (selectedCategory === "All" || item.category === selectedCategory) &&
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  });
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList 
+        items={filteredItems} 
+        onItemFormSubmit={handleItemFormSubmit}
+        onCategoryChange={(e) => setSelectedCategory(e.target.value)}
+        onSearchChange={(e) => setSearchText(e.target.value)}
+        searchText={searchText}
+      />
     </div>
   );
 }
